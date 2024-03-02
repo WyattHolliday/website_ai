c03b97f5762d6cdaae93d070d8628e21908ca059
JosephBalaty <balatyj@oregonstate.edu>
2024-03-02 14:41:12 -0800

    update branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import GameDisplay from './GameDisplay';
 import Tictactoe from './Tictactoe';
 import empty_pfp from './img/user_avatar.png';
 import tictactoe_img from './img/tictactoe.png';
-// import connect4_img from './img/connect4.png';
+import connect4_img from './img/connect4.png';
+import Connect4 from './Connect4';
 import './App.css';
 
 function App() {
@@ -13,14 +14,17 @@ function App() {
         <h1 className='header-text'>Board Game Hub</h1>
         <img src={empty_pfp} className="pfp-img" alt="profile" />
       </header>
-      <Routes>
-        <Route path="/" element={<GameDisplay gameName="Tic tac toe" gameDescription="Play tic tac toe against opponents in offline matches!" gameImg={tictactoe_img} path="/Tictactoe" />} />
-        <Route path="/Tictactoe" element={<Tictactoe />} />
 
-        <Route path="/" element={<GameDisplay gameName="Connect 4" gameDescription="Play Connect 4 against opponents in offline matches!" gameImg={connect4_img} path="/Connect4" />} />
-        <Route path="/Connect4" element={<Connect4 />} />
+      <GameDisplay gameName="Connect 4" gameDescription="Play Connect4 against opponents in offline matches!" gameImg={connect4_img} path="Connect4" />
+      <GameDisplay gameName="Tic tac toe" gameDescription="Play tic tac toe against opponents in offline matches!" gameImg={tictactoe_img} path="Tictactoe" />
+
+      <Routes>
+        <Route path="/">
+          <Route path="/Connect4" element={<Connect4 />} />
+          <Route path="/Tictactoe" element={<Tictactoe />} />
+        </Route>
       </Routes>
-    </Router>
+    </Router >
   );
 }
 
