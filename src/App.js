608a726d32c1f13fe9d4250bb9ee4a85f16ab015
JosephBalaty <balatyj@oregonstate.edu>
2024-03-02 13:10:44 -0800

    added bootstrapping

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ function App() {
         <h1 className='header-text'>Board Game Hub</h1>
         <img src={empty_pfp} className="pfp-img" alt="profile" />
       </header>
-      <GameDisplay 
-        gameName="Tic Tac Toe" 
-        gameDescription="Play tic tac toe against opponents in offline matches!" 
-        gameImg={tictactoe_img} 
+      <GameDisplay
+        gameName="Tic Tac Toe"
+        gameDescription="Play tic tac toe against opponents in offline matches!"
+        gameImg={tictactoe_img}
       />
-      <GameDisplay 
-        gameName="Connect 4" 
-        gameDescription="Drop your discs into the grid and get four in a row to win!" 
-        gameImg={connect4_img} 
+      <GameDisplay
+        gameName="Connect 4"
+        gameDescription="Drop your discs into the grid and get four in a row to win!"
+        gameImg={connect4_img}
       />
     </>
   );
