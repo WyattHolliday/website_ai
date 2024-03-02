fc966cfc60b8ee33a072ff8f00a714db20d2c163
WyattHolliday <wph03@icloud.com>
2024-03-02 13:11:59 -0800

    Added new page functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GameDisplay from './GameDisplay';
+import Tictactoe from './Tictactoe';
 import empty_pfp from './img/user_avatar.png';
 import tictactoe_img from './img/tictactoe.png';
-import connect4_img from './img/connect4.png';
+// import connect4_img from './img/connect4.png';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <header className="App-header">
         <h1 className='header-text'>Board Game Hub</h1>
         <img src={empty_pfp} className="pfp-img" alt="profile" />
       </header>
-      <GameDisplay 
-        gameName="Tic Tac Toe" 
-        gameDescription="Play tic tac toe against opponents in offline matches!" 
-        gameImg={tictactoe_img} 
-      />
-      <GameDisplay 
-        gameName="Connect 4" 
-        gameDescription="Play connect 4 against opponents in offline matches!" 
-        gameImg={connect4_img} 
-      />
-    </>
+      <Routes>
+        <Route path="/" element={<GameDisplay gameName="Tic Tac Toe" gameDescription="Play tic tac toe against opponents in offline matches!" gameImg={tictactoe_img} path="/Tictactoe" />} />
+        <Route path="/Tictactoe" element={<Tictactoe />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/GameDisplay.js
+++ src/GameDisplay.js
@@ -1,9 +1,11 @@
-function GameDisplay({ gameName, gameDescription, gameImg }) {
+import { Link } from 'react-router-dom';
+
+function GameDisplay({ gameName, gameDescription, gameImg, path }) {
   return (
     <div className="game-display">
         <div className="game-text-container">
             <h2 className="game-text">{gameName}</h2>
-            <p className="game-text">{gameDescription}</p>
+            <Link to={path} className="game-text">{gameDescription}</Link>
         </div>
         <img className="game-display-img" src={gameImg} alt={gameName} />
     </div>
