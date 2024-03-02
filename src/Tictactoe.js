6a8b2c17c26845b0076f526c17e2a8b2e357ad29
WyattHolliday <wph03@icloud.com>
2024-03-02 14:36:35 -0800

    basic tictactoe board

--- src/Tictactoe.js
+++ src/Tictactoe.js
@@ -1,4 +1,6 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Tictactoe.css';
 
 function Tictactoe() {
   let navigate = useNavigate();
@@ -7,11 +9,41 @@ function Tictactoe() {
     navigate('/', { replace: true });
   }
 
+  const [board] = useState(Array(9).fill(null));
+
+  const renderSquare = (i) => {
+    return (
+      // onClick={() => handleClick(i)}>
+      <button className="square col">
+        {board[i]}
+      </button>
+    );
+  };
+
   return (
     <>
-      <h1>Tic Tac Toe</h1>
-      <p>Play tic tac toe against opponents in offline matches!</p>
-      <button onClick={handleBackToHome}>Back to Home</button>
+      <h1 className='text-center'>Tic Tac Toe</h1>
+      <div className='board col-6 mx-auto'>
+        {/* <div className="status">{getStatus()}</div> */}
+        <div className="board-row row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className='button-container text-center'>
+        <button className='home-button' onClick={handleBackToHome}>Back to Home</button>
+      </div>
     </>
   );
 }
