34d77365185567b1d2301f1841066f8e9d6970f2
JosephBalaty <balatyj@oregonstate.edu>
2024-03-02 13:55:01 -0800

    Add connect 4 routing elements to app.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<GameDisplay gameName="Tic tac toe" gameDescription="Play tic tac toe against opponents in offline matches!" gameImg={tictactoe_img} path="/Tictactoe" />} />
         <Route path="/Tictactoe" element={<Tictactoe />} />
+
+        <Route path="/" element={<GameDisplay gameName="Connect 4" gameDescription="Play Connect 4 against opponents in offline matches!" gameImg={connect4_img} path="/Connect4" />} />
+        <Route path="/Connect4" element={<Connect4 />} />
       </Routes>
     </Router>
   );
